6bdb0408dcc61ea52e622013d56518409f161951
tatsuya.kurasawa <tatsuya.kurasawa@g.dreamcareer.co.jp>
2025-04-08 16:52:02 +0900

    iss0006修正

--- database/seeders/LaravelFormsSeeder.php
+++ database/seeders/LaravelFormsSeeder.php
@@ -14,11 +14,11 @@ class LaravelFormsSeeder extends Seeder
     public function run(): void
     {
         DB::table('laravel_forms')->insert([
-            ['フォームID' => 1, '名前' => 'テスト1', 'メールアドレス' => 'test1@com', '年齢' => 10, '技術ID' => 1, '出身' => '東京'],
-            ['フォームID' => 2, '名前' => 'テスト2', 'メールアドレス' => 'test2@com', '年齢' => 20, '技術ID' => 3, '出身' => '神奈川'],
-            ['フォームID' => 3, '名前' => 'テスト3', 'メールアドレス' => 'test3@com', '年齢' => 30, '技術ID' => 2, '出身' => '埼玉'],
-            ['フォームID' => 4, '名前' => 'テスト4', 'メールアドレス' => 'test4@com', '年齢' => 40, '技術ID' => 5, '出身' => '千葉'],
-            ['フォームID' => 5, '名前' => 'テスト5', 'メールアドレス' => 'test5@com', '年齢' => 50, '技術ID' => 4, '出身' => null],
+            ['formID' => 1, 'name' => 'テスト1', 'mailaddres' => 'test1@com', 'age' => 10, 'techID' => 1, 'from' => '東京'],
+            ['formID' => 2, 'name' => 'テスト2', 'mailaddres' => 'test2@com', 'age' => 20, 'techID' => 3, 'from' => '神奈川'],
+            ['formID' => 3, 'name' => 'テスト3', 'mailaddres' => 'test3@com', 'age' => 30, 'techID' => 2, 'from' => '埼玉'],
+            ['formID' => 4, 'name' => 'テスト4', 'mailaddres' => 'test4@com', 'age' => 40, 'techID' => 5, 'from' => '千葉'],
+            ['formID' => 5, 'name' => 'テスト5', 'mailaddres' => 'test5@com', 'age' => 50, 'techID' => 4, 'from' => null],
         ]);
     }
 }
--- database/seeders/LaravelTechnologiesSeeder.php
+++ database/seeders/LaravelTechnologiesSeeder.php
@@ -14,11 +14,11 @@ class LaravelTechnologiesSeeder extends Seeder
     public function run(): void
     {
         DB::table('laravel_technologies')->insert([
-            ['技術ID' => 1, '得意な言語' => 'HTML'],
-            ['技術ID' => 2, '得意な言語' => 'PHP'],
-            ['技術ID' => 3, '得意な言語' => 'CSS'],
-            ['技術ID' => 4, '得意な言語' => 'JAVA'],
-            ['技術ID' => 5, '得意な言語' => 'Ruby'],
+            ['techID' => 1, 'lang' => 'HTML'],
+            ['techID' => 2, 'lang' => 'PHP'],
+            ['techID' => 3, 'lang' => 'CSS'],
+            ['techID' => 4, 'lang' => 'JAVA'],
+            ['techID' => 5, 'lang' => 'Ruby'],
         ]);
     }
 }
