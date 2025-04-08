2510a2b94ce554d9c95033c2a973bb4557502be5
tatsuya.kurasawa <tatsuya.kurasawa@g.dreamcareer.co.jp>
2025-04-08 18:18:07 +0900

    iss6 file追加修正

---
+++ database/migrations/2025_04_08_033634_create_laravel_technologies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('laravel_technologies', function (Blueprint $table) {
+            $table->id('技術ID');
+            $table->string('得意な言語');
+            $table->timestamps();
+
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laravel_technologies');
+    }
+};
