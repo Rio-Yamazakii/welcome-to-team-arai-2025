cf0c8f0542942ed6c61fe7184384bbc56a31ff87
tatsuya.kurasawa <tatsuya.kurasawa@g.dreamcareer.co.jp>
2025-04-08 18:51:40 +0900

    一行誤削除したのでpushします

--- database/seeders/LaravelFormSeeder.php
+++ database/seeders/LaravelFormSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class LaravelFormsSeeder extends Seeder
