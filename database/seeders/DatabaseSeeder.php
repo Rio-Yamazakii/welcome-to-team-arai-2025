bfebca56563e6fabdcf6bf9d4136a63772776cc8
shota.kudo <shota.kudo@g.dreamcareer.co.jp>
2025-04-08 18:09:38 +0900

    0007_1809

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,11 +8,8 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void    {
-
+    public function run(): void
+    {
         $this->call([
             FormsTableSeeder::class,
             TechnologiesTableSeeder::class,
