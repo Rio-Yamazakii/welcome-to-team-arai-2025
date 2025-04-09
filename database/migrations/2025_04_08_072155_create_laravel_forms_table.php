71e33e521c5a660170cebed83e912beaa3e829ba
akihiro.tada <akihiro.tada@g.dreamcareer.co.jp>
2025-04-09 11:27:00 +0900

    iss0006_Tada

---
+++ database/migrations/2025_04_08_072155_create_laravel_forms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('laravel_forms', function (Blueprint $table) {
+            $table->id('form_id'); // フォームID
+            $table->string('name');
+            $table->string('email');
+            $table->integer('age')->nullable();
+            $table->unsignedBigInteger('tech_id')->nullable();
+            $table->string('form')->nullable();
+            $table->timestamps();
+    
+            // 文字コード設定
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_general_ci';
+    
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laravel_forms');
+    }
+};
