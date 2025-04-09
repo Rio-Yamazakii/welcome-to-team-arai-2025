7fb33ffac7cc10a0d9bba373c3fe8c1d223b5db0
shota.kudo <shota.kudo@g.dreamcareer.co.jp>
2025-04-09 13:03:13 +0900

    0014_1303

---
+++ resources/js/components/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Welcome = () => {
+    return <h1 style={{ color: 'black' }}>はろー　りあくと！</h1>;
+}
+
+export default Welcome;
---
+++ resources/views/welcome-react.blade.php
@@ -0,0 +1,20 @@
+<!-- resources/views/welcome-react.blade.php -->
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>React Example</title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx'])
+</head>
+
+<body>
+    <!-- Reactと連携できてないよ！^_^ -->
+    <div id="0014_react"></div>
+</body>
+
+</html>
