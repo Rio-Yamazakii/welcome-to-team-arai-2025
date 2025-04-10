ccd14de3fcd86bc0d3623a19ccd96f0c50362af2
daisuke.yanai <daisuke.yanai@g.dreamcareer.co.jp>
2025-04-10 15:18:28 +0900

    iss0015_yanai 再度提出です

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel + React</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-        @viteReactRefresh
+    @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
