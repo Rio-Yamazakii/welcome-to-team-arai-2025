def5ae1816f3bced8edeabefd5a992c680a63879
daisuke.yanai <daisuke.yanai@g.dreamcareer.co.jp>
2025-04-10 15:12:38 +0900

    iss0015_yanai　再提出

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -1,29 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel + React</title>
-    
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-    <div id="react-app"></div>
-</body>
-</html> -->
-
-<!-- <!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>テストページ</title>
-</head>
-<body>
-    <h1>これはテストページです</h1>
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
