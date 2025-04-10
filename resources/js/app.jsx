6d8402cb6ef73a5a6f01957362230a83314f7a5e
shota.kudo <shota.kudo@g.dreamcareer.co.jp>
2025-04-10 12:55:06 +0900

    0017_不要行の修正

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Welcome from './components/Welcome';  // コンポーネントをインポート
 import Forms from './components/Forms';  // #17
 
-// const root = ReactDOM.createRoot(document.getElementById("0014_react"));
-// root.render(<Welcome />);  // Welcomeコンポーネントを#0014_reactにレンダリング
 const root = ReactDOM.createRoot(document.getElementById("0018_react"));
 root.render(<Forms />);
-
-// export default App;
