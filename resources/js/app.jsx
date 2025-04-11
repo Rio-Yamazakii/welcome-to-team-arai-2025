6ee59e0e882cb89a908d1545d54dcd729c38e085
akihiro.tada <akihiro.tada@g.dreamcareer.co.jp>
2025-04-11 11:10:07 +0900

    iss0015_Tada

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import HelloChild from './components/HelloChild';
 
 const root = ReactDOM.createRoot(document.getElementById('react-root'));
-root.render(<React.StrictMode>
+root.render(
+            <React.StrictMode>
                 <HelloChild name="Akihiro" />
             </React.StrictMode>);
