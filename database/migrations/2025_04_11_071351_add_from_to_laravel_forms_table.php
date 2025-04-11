774195a8d98776d79e4d7cef12fe5ea4fe6cad9e
seika.nakano <seika.nakano@g.dreamcareer.co.jp>
2025-04-11 18:43:48 +0900

    マイグレーションのロールアップ処理の追加

--- database/migrations/2025_04_11_071351_add_from_to_laravel_forms_table.php
+++ database/migrations/2025_04_11_071351_add_from_to_laravel_forms_table.php
@@ -21,7 +21,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('laravel_forms', function (Blueprint $table) {
-            //
+            $table->dropColumn('from');//
         });
     }
 };
