dacae5d4fe5a1ce96c61ca911ccea18870e799e0
ishihara yoshihito <yoshihito.ishihara@g.dreamcareer.co.jp>
2025-04-11 10:33:46 +0900

    issue#8修正

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/form', [FormController::class, 'index'])->name('form.index');;
+Route::get('/form', [FormController::class, 'index'])->name('form.index');
 Route::get('/form/create', [FormController::class, 'create'])->name('form.create');
 Route::post('/form', [FormController::class, 'store'])->name('form.store');
 Route::post('/form/{id}/update', [FormController::class, 'update'])->name('form.update');
