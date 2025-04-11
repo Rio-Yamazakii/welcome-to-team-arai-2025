f87e321e6e2fcd05ab051b27a2cd7cc8427f35b2
seri.otsuka <seri.otsuka@g.dreamcareer.co.jp>
2025-04-11 11:46:21 +0900

    #16-2回目の修正を行いました

--- resources/js/components/ChildComponent.jsx
+++ resources/js/components/ChildComponent.jsx
@@ -7,36 +7,36 @@ const ChildComponent = (props) => {
     return (
         <div className="p-8">
             <h2 className="text-3xl font-bold pb-8">{props.title}</h2>
-            <div class="relative overflow-x-auto">
-            <table class="w-96 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div className="relative overflow-x-auto">
+            <table className="w-96 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             id
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" classNames="px-6 py-3">
                             名前
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             年齢
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             出身地
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             1
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             太郎
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             25
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             新潟
                         </td>
                     </tr>
