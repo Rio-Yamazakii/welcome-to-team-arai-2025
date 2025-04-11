9022b8605f311fbf4c4b16df6db3a68a46856790
akihiro.tada <akihiro.tada@g.dreamcareer.co.jp>
2025-04-11 10:45:22 +0900

    iss0015_Tada

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,7 @@
 import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HelloReact from './components/HelloReact';
+import HelloChild from './components/HelloChild';
 
 const root = ReactDOM.createRoot(document.getElementById('react-root'));
-root.render(<HelloReact />);
+root.render(<HelloChild name="Akihiro" />);
