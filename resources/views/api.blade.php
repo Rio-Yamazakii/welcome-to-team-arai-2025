7e33bbfb801e707a62186ddd11c4a029bbe8f2e9
ishihara yoshihito <yoshihito.ishihara@g.dreamcareer.co.jp>
2025-04-11 16:29:20 +0900

    #18finish

---
+++ resources/views/api.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>React 表示</title>
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx'])
+</head>
+<body>
+    <div id="example"></div>
+</body>
+</html>
